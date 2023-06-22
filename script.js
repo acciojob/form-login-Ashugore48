dda8e8e5d87a41640c34adb76bb6fb3968f9dce0
Ashutosh Sunil Gore <130917119+Ashugore48@users.noreply.github.com>
2023-06-22 09:48:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	const fname = document.getElementById('form1').elements[0].value;
+	const lname = document.getElementById('form1').elements[1].value;
+	// console.log(fname);
+	alert(fname+' '+lname);
 
 }
+getFormvalue();
